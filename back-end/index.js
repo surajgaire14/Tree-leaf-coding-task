070e24afca57fbf9cf4970d95c0f8ed568a03bd6
Suraj Gaire <eriag321@gmail.com>
2024-04-25 11:04:45 +0545

    ..

--- back-end/index.js
+++ back-end/index.js
@@ -25,20 +25,20 @@ app
   })
   .use("/api/user", UserRouter)
   .use("/uploads",express.static(__dirname + "uploads"))
-  .use("/uploads/:filename",(req,res) => {
-    const baseUrl = process.env.APP_URL; 
-    // console.log(req.params.filename)
-    const encodedFilename = encodeURIComponent(req.params.filename);
-    const filePath = new URL(`uploads/${encodeURIComponent(req.params.filename)}`,baseUrl.toString())
-    res.sendFile(filePath, (err) => {
-      if (err) {
-          console.log('Error sending file:', err);
-          if (!res.headersSent) {
-              res.status(404).send("File not found");
-          }
-      }
-  });
-  })
+  // .use("/uploads/:filename",(req,res) => {
+  //   const baseUrl = process.env.APP_URL; 
+  //   // console.log(req.params.filename)
+  //   const encodedFilename = encodeURIComponent(req.params.filename);
+  //   const filePath = new URL(`uploads/${encodeURIComponent(req.params.filename)}`,baseUrl.toString())
+  //   res.sendFile(filePath, (err) => {
+  //     if (err) {
+  //         console.log('Error sending file:', err);
+  //         if (!res.headersSent) {
+  //             res.status(404).send("File not found");
+  //         }
+  //     }
+  // });
+  // })
   .get("*",(req,res) => {
     return res.send("Sorry,Route not found")
   })
